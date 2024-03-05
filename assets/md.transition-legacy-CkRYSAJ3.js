3bf93f1dd1a78800d48044dcbaba99ae3d25896a
Cliffmeister <dev.clfm5t12@gmail.com>
2024-03-05 14:35:14 +0800

    Updates

--- assets/md.transition-legacy-CkRYSAJ3.js
+++ assets/md.transition-legacy-CkRYSAJ3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Lezvb_E-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-D5HOrc4r.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
